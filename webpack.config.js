733d3727d69d3d6e8daea42c4da96a4d41caec78
Lelouch <gledich@tut.by>
2017-04-26 13:21:46 +0300

    Changes in config files.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : false,
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new ExtractTextPlugin('css/bundle.css'),
+        new ExtractTextPlugin('bundle.css'),
         new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(NODE_ENV)
         })
@@ -42,39 +42,43 @@ module.exports = {
         historyApiFallback: true
     },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel-loader'],
-                include: [
-                    path.resolve(__dirname, 'src')
-                ]
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                loader: 'file?name=img/[path][name].[ext]'
-            },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file?name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
-            }
+        rules: [{
+            test: /\.(css|scss)$/,
+            use: ExtractTextPlugin.extract({
+              fallback:'style-loader',
+              use:'css-loader?minimize=true!sass-loader?resolve url'
+            }),
+            include: /src/
+          },
+          {
+            test: /\.js$/,
+            use: ['react-hot', 'babel-loader'],
+            include: /src/
+          },
+          {
+            test: /\.(png|jpg|svg|gif)$/,
+            use: 'file?name=img/[path][name].[ext]'
+          },
+          {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            use: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+          },
+          {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            use: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+          },
+          {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            use: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+          },
+          {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            use: 'file?name=fonts/[name].[ext]'
+          },
+          {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            use: 'url?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+          }
         ]
     }
 };
